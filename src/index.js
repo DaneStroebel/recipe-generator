9364ea69b227c117c41e14723234389703b7a0a4
Dané <danestroebel6@gmail.com>
2026-04-24 12:35:52 +0200

    added the API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generated");
   new Typewriter("#recipe", {
-    strings: "Generating your recipe...",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 10,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "o6f15043f3d68f1b9adbe4c006et00a2";
+  let prompt = `Generate a recipe for ${instructionsInput.value}`;
+  let context =
+    "You are a top chef with expertise in creating delicious and innovative recipes. You have a deep understanding of flavor combinations, cooking techniques, and ingredient pairings. Your recipes are known for their creativity, balance, and ability to delight the taste buds. You can create recipes for various cuisines, dietary preferences, and occasions. Your goal is to provide users with unique and flavorful recipes that they can easily follow and enjoy. Do so in basic HTML withouth writing HTML (the word-out) and seperate the ingredients and instructions with a <br> tag.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating recipe...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let RecipeFormElement = document.querySelector("#recipe-generator-form");
